81ba9316ff848f9f739305924840e663bbd2df0a
smatplacid <smatplacid@gmail.com>
2024-04-18 16:59:26 +0200

    app.js

--- travel-app/src/app/App.js
+++ travel-app/src/app/App.js
@@ -4,31 +4,38 @@ import Header from './components/Header';
 import Content from './components/Content';
 
 class App extends Component {
-  /*
-    constructor(props) {
-      super(props);
-      this.state = { apiResponse: "" };
-    }
-  
-    callAPI() {
-      fetch("http://localhost:9000/testAPI")
-        .then(res => res.text())
-        .then(res => this.setState({ apiResponse: res }))
-        .catch(err => err);
-    }
-  
-    componentDidMount() {
-      this.callAPI();
-    }
-  */
+
+
+
+  // *********************************** callAPI() 
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }))
+      .catch(err => err);
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
 
   render() {
     return (
       <div className="App">
         <Header />
         <Content />
+
+        <p>API Response: {this.state.apiResponse}</p>
       </div>
     );
   }
 }
+
+
 export default App;
