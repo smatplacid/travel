293e854dbedc387ad37b65b89c98494c91f3a90f
sami-born <sarah.born@edu.techstarter.de>
2024-04-23 10:52:32 +0200

    output

--- travel-app/src/app/App.js
+++ travel-app/src/app/App.js
@@ -34,8 +34,8 @@ class App extends Component {
       <div className="App">
         
         <Header />
-        {/*<Content />*/}
-        <Translater />
+        {/* <Content /> */}
+        <Translater /> 
 
         <Router>
           <Fragment>
