b91b2b693e9427d53cba483c202c2c58b4fcd025
Death-Knight75 <liebesgott1975@googlemail.com>
2024-04-25 13:07:26 +0200

    data,js to JSON

--- travel-app/src/app/data.js
+++ travel-app/src/app/data.js
@@ -13,6 +13,7 @@
 //     "bs-BA": "Bosnian",
 // }
 
+
 // export default countries;
 
 const countries = {
@@ -28,6 +29,91 @@ const countries = {
         name: "English",
         image: 'https://hatscripts.github.io/circle-flags/flags/gb-eng.svg'
     },
+    "cz": {
+        name: "Czechia",
+        image: 'https://hatscripts.github.io/circle-flags/flags/cz.svg'
+    },
+    "br": {
+        name: "Brazil",
+        image: 'https://hatscripts.github.io/circle-flags/flags/br.svg'
+    },
+    "be": {
+        name: "Belgium",
+        image: 'https://hatscripts.github.io/circle-flags/flags/be.svg'
+    },
+    "ae": {
+        name: "United Arab Emirates",
+        image: 'https://hatscripts.github.io/circle-flags/flags/ae.svg'
+    },
+    "cn": {
+        name: "China",
+        image: 'https://hatscripts.github.io/circle-flags/flags/cn.svg'
+    },
+    "co": {
+        name: "Colombia",
+        image: 'https://hatscripts.github.io/circle-flags/flags/co.svg'
+    },
+    "dk": {
+        name: "Denmark",
+        image: 'https://hatscripts.github.io/circle-flags/flags/dk.svg'
+    },
+    "es": {
+        name: "Spain",
+        image: 'https://hatscripts.github.io/circle-flags/flags/es.svg'
+    },
+    "fr": {
+        name: "France",
+        image: 'https://hatscripts.github.io/circle-flags/flags/fr.svg'
+    },
+    "fi": {
+        name: "Finland",
+        image: 'https://hatscripts.github.io/circle-flags/flags/fi.svg'
+    },
+    "gr": {
+        name: "Greece",
+        image: 'https://hatscripts.github.io/circle-flags/flags/gr.svg'
+    },
+    "id": {
+        name: "Indonesia",
+        image: 'https://hatscripts.github.io/circle-flags/flags/id.svg'
+    },
+    "il": {
+        name: "Israel",
+        image: 'https://hatscripts.github.io/circle-flags/flags/il.svg'
+    },
+    "in": {
+        name: "India",
+        image: 'https://hatscripts.github.io/circle-flags/flags/in.svg'
+    },
+    "it": {
+        name: "Italy",
+        image: 'https://hatscripts.github.io/circle-flags/flags/it.svg'
+    },
+    "jp": {
+        name: "Japan",
+        image: 'https://hatscripts.github.io/circle-flags/flags/jp.svg'
+    },
+    "kr": {
+        name: "South Korea",
+        image: 'https://hatscripts.github.io/circle-flags/flags/kr.svg'
+    },
+    "mx": {
+        name: "Mexico",
+        image: 'https://hatscripts.github.io/circle-flags/flags/mx.svg'
+    },
+    "my": {
+        name: "Malaysia",
+        image: 'https://hatscripts.github.io/circle-flags/flags/my.svg'
+    },
+    "nl": {
+        name: "Netherlands",
+        image: 'https://hatscripts.github.io/circle-flags/flags/nl.svg'
+    },
+    "sv": {
+        name: "Sweden",
+        image: 'https://hatscripts.github.io/circle-flags/flags/sv.svg'
+    },
+
 
 };
 
